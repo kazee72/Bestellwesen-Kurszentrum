ece11b2305af3811d4f87030f1fcbebd3cc95ebf
jonathan santis <jonathan.wyss@santismail.ch>
2025-03-17 17:05:04 +0100

    this is a broken commit

--- AppRoot/app.py
+++ AppRoot/app.py
@@ -2,7 +2,19 @@ from flask import Flask
 from Home.routes import home_bp
 from Dashboard.routes import dashboard_bp
 from Login.routes import login_bp
-from flask 
+from flask_sqlalchemy import SQLAlchemy
+from Login import models
+
+def initDb(application):
+    db = SQLAlchemy()
+    application.config["SQLALCHEMY_DATBASE_URI"] = 'sqlite://db.sqlite'
+    return db
+
+def testAdduser(db):
+    email = "dooner@dooner.ch"
+    name = "testAdduser"
+    pw = "anothersafepassword"
+    models.User(email=email,name=name,passowrd=pw)
 
 app = Flask("bööö")
 
@@ -11,7 +23,8 @@ app.register_blueprint(home_bp,url_prefix='/home')
 app.register_blueprint(login_bp,url_prefix="/login")
 app.register_blueprint(dashboard_bp,url_prefix='/dashboard')
 
-db = SQLAl
+db = initDb(app)
+testAdduser(db)
 
 if __name__ == '__main__':
     app.run(debug=True)
