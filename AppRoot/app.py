6067281568bec7284dc162338d2f2f1e5c5e6908
jonathan santis <jonathan.wyss@santismail.ch>
2025-03-17 16:18:03 +0100

    Import sqlalchemy

--- AppRoot/app.py
+++ AppRoot/app.py
@@ -2,15 +2,16 @@ from flask import Flask
 from Home.routes import home_bp
 from Dashboard.routes import dashboard_bp
 from Login.routes import login_bp
+from flask 
 
 app = Flask("bööö")
 
 app.register_blueprint(home_bp,url_prefix='/',name="home-red")
 app.register_blueprint(home_bp,url_prefix='/home')
-
 app.register_blueprint(login_bp,url_prefix="/login")
-
 app.register_blueprint(dashboard_bp,url_prefix='/dashboard')
+
+db = SQLAl
 
 if __name__ == '__main__':
     app.run(debug=True)
