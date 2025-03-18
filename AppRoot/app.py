7bb076da763b18d22cc998dd748a6c07142053ef
jonathan santis <jonathan.wyss@santismail.ch>
2025-03-18 14:52:50 +0100

    workaround circular imports

--- AppRoot/app.py
+++ AppRoot/app.py
@@ -1,30 +1,34 @@
 from flask import Flask
-from Home.routes import home_bp
-from Dashboard.routes import dashboard_bp
-from Login.routes import login_bp
 from flask_sqlalchemy import SQLAlchemy
-from Login import models
+from .Home.routes import home_bp
+from .Dashboard.routes import dashboard_bp
+from .Login.routes import login_bp
 
-def initDb(application):
-    db = SQLAlchemy()
-    application.config["SQLALCHEMY_DATBASE_URI"] = 'sqlite://db.sqlite'
-    return db
 
-def testAdduser(db):
+
+app = Flask("bööö")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite://db.sqlite'
+
+from . import database
+db = database.initDb(app)
+from .Login import models
+
+def testAdduser():
     email = "dooner@dooner.ch"
     name = "testAdduser"
     pw = "anothersafepassword"
-    models.User(email=email,name=name,passowrd=pw)
-
-app = Flask("bööö")
+    models.User(email=email,name=name,password=pw)
 
 app.register_blueprint(home_bp,url_prefix='/',name="home-red")
 app.register_blueprint(home_bp,url_prefix='/home')
 app.register_blueprint(login_bp,url_prefix="/login")
 app.register_blueprint(dashboard_bp,url_prefix='/dashboard')
 
-db = initDb(app)
-testAdduser(db)
-
+testAdduser()
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
