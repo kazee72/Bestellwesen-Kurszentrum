474ec712271a46100b7b83eec30c86c5fc11fc07
jonathan santis <jonathan.wyss@santismail.ch>
2025-03-19 10:37:40 +0100

    add static url in template

--- AppRoot/Home/routes.py
+++ AppRoot/Home/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from flask import render_template
 
-home_bp = Blueprint('home_b',
+home_bp = Blueprint('home_bp',
                     __name__,
                     template_folder="templates",
                     static_folder="static")
