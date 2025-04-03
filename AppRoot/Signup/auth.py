28992a8cc343cac725b08c0493fc076acf6f2446
Joel Bonini <joel.bonini@gmx.ch>
2025-04-03 09:11:41 +0200

    push missing files

---
+++ AppRoot/Signup/auth.py
@@ -0,0 +1,36 @@
+from flask import redirect, request, flash, url_for
+import flask
+from werkzeug.security import check_password_hash, generate_password_hash
+from AppRoot import app
+from sqlalchemy import exc
+from AppRoot.Login import models
+from AppRoot import database
+
+def signUp():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    repeatPassword = request.form.get('repeatPassword')
+
+    user = models.User.query.filter_by(username=username).first()
+    if user:
+       flash("this username is already taken")
+       return redirect(url_for('login_bp.signUpPage'))
+    else:
+        if password == repeatPassword and len(password) > 10:
+            newUser = models.User(username=username,
+                                  password=generate_password_hash(password,method="scrypt"))
+            with app.app.app_context():
+                database.db.session.add(newUser)
+                try:
+                    database.db.session.commit()
+                except exc.IntegrityError as e:
+                    app.logger.info("User already exist in db:"+ str(e))
+                    flash("Error adding user!, please contact your sysadmin")
+                    database.db.session.rollback()
+                else:
+                    flash("User successfull added")
+        else:
+           flash("password is invalid")
+           return redirect(url_for('login_bp.signUpPage'))
+
+        return redirect("/login")
---
+++ AppRoot/Signup/models.py
@@ -0,0 +1,9 @@
+from ..database import db as userDb
+
+class User(userDb.Model):
+    uid = userDb.Column(userDb.Integer,primary_key=True)
+    email = userDb.Column(userDb.String(100))
+    password = userDb.Column(userDb.String(100))
+    username = userDb.Column(userDb.String(100),unique=True)
+    firstname = userDb.Column(userDb.String(100))
+    lastname = userDb.Column(userDb.String(100))
