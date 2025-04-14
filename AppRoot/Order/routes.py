cb81d7dc431716ba692b137ed002393ee683b233
jonathan santis <jonathan.wyss@santismail.ch>
2025-04-14 10:42:49 +0200

    move some imports for better overview and to prevent circular imports

--- AppRoot/Order/routes.py
+++ AppRoot/Order/routes.py
@@ -1,4 +1,6 @@
 import flask
+from AppRoot.Order import models
+from AppRoot.app import logger
 
 order_bp = flask.Blueprint("order_bp",
                            __name__,
@@ -8,3 +10,11 @@ order_bp = flask.Blueprint("order_bp",
 @order_bp.route("/",methods=["GET"])
 def index():
     return flask.render_template("IndexOrders.html")
+
+@order_bp.route("/addOrder",methods=["GET","POST"])
+def addOrder():
+    try:
+        itemList = models.ItemStore.query.all()
+    except Exception as e:
+        logger.error("Eerror at db call for itemstore:\n",e)
+    return flask.render_template(template_name_or_list="AddOrder.html",items=itemList)
